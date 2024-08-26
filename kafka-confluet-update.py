c6c0567ab0c9effc09bfc683e0f75b3925562d21
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-25 22:20:18 -0300

    Fix

--- kafka-confluet-update.py
+++ kafka-confluet-update.py
@@ -1,28 +1,40 @@
 import argparse
 from confluent_kafka.admin import AdminClient, ConfigResource, ConfigEntry, AlterConfigOpType
+from confluent_kafka.error import KafkaException
 
 def get_config(admin_client, topic_name, config_name):
-    resource = ConfigResource('topic', topic_name)
-    result_dict = admin_client.describe_configs([resource])
-    config_entries = result_dict[resource].result()
-    if config_name in config_entries:
-        config_property = config_entries[config_name]
-        return config_property.value
-    else:
-        raise ValueError(f"Configuration {config_name} not found for topic {topic_name}")
-
+    
+    try:
+        resource = ConfigResource('topic', topic_name)
+        result_dict = admin_client.describe_configs([resource])
+        config_entries = result_dict[resource].result()
+        if config_name in config_entries:
+            config_property = config_entries[config_name]
+            return config_property.value
+        else:
+            raise ValueError(f"Configuration {config_name} not found for topic {topic_name}")
+    except KafkaException as e:
+        print(f"Error while trying to get the configuration: {e}")
+        raise
+    
 def set_config(admin_client, topic_name, config_dicts):
-    # Create ConfigEntry objects for each config to set
-    config_entries = [ConfigEntry(name=name, value=str(value), incremental_operation=AlterConfigOpType['SET'])
-                      for name, value in config_dicts.items()]
-    
-    # Create ConfigResource with the list of configurations
-    resource = ConfigResource('topic', topic_name, incremental_configs=config_entries)
-    
-    result_dict = admin_client.incremental_alter_configs([resource])
-    result_dict[resource].result()  # Wait for the result to ensure the configuration is applied
+
+    try:
+        # Create ConfigEntry objects for each config to set
+        config_entries = [ConfigEntry(name=name, value=str(value), incremental_operation=AlterConfigOpType['SET'])
+                        for name, value in config_dicts.items()]
+        
+        # Create ConfigResource with the list of configurations
+        resource = ConfigResource('topic', topic_name, incremental_configs=config_entries)
+        
+        result_dict = admin_client.incremental_alter_configs([resource])
+        result_dict[resource].result()  # Wait for the result to ensure the configuration is applied
+    except KafkaException as e:
+        print(f"Error while trying to set the configuration: {e}")
+        raise
 
 def main():
+
     # Configura o parser de argumentos
     parser = argparse.ArgumentParser(description='Atualiza a configuração de um tópico do Kafka.')
     parser.add_argument('topic_name', type=str, help='Nome do tópico')
@@ -39,14 +51,16 @@ def main():
         'retention.ms': 10,
         'cleanup.policy': 'compact'
     }
-    
-    # Define múltiplas configurações
-    set_config(admin_client, topic_name, config_dicts)
-    
-    # Verifica se as propriedades de configuração foram atualizadas
-    for config_name in config_dicts.keys():
-        new_value = get_config(admin_client, topic_name, config_name)
-        print(f'Now {config_name} for topic {topic_name} is {new_value}')
+    try:
+        # Define múltiplas configurações
+        set_config(admin_client, topic_name, config_dicts)
+        
+        # Verifica se as propriedades de configuração foram atualizadas
+        for config_name in config_dicts.keys():
+            new_value = get_config(admin_client, topic_name, config_name)
+            print(f'Now {config_name} for topic {topic_name} is {new_value}')
+    except Exception as e:
+        print(f"Error while trying to update the topic configuration: {e}")
 
 if __name__ == '__main__':
     main()
