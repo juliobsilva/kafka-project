abfa862efe1ad30b2672ccf61513e8dafc6327a2
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-25 23:52:47 -0300

    Fix

--- kafka-confluent-delete.py
+++ kafka-confluent-delete.py
@@ -1,12 +1,29 @@
 import sys
 import argparse
 from confluent_kafka.admin import AdminClient
+from confluent_kafka.error import KafkaException
 
 def delete_topic(admin_client, topic_name):
-    try:        
-        admin_client.delete_topics([topic_name])
-        print(f'Tópico "{topic_name}" deletado com sucesso.')
-        return
+    try:
+        # Verifica se o tópico já existe
+        metadata = admin_client.list_topics(timeout=10)
+        if topic_name not in metadata.topics:
+            print(f'O tópico "{topic_name}" não existe.')
+            return 1
+        
+        # Deletar o tópico
+        futures = admin_client.delete_topics([topic_name])
+        
+        # Aguarde a conclusão da deleção
+        for topic, future in futures.items():
+            try:
+                future.result()  # Bloqueia até a deleção ser concluída
+                print(f'Tópico "{topic}" deletado com sucesso.')
+            except KafkaException as e:
+                print(f'Erro ao deletar o tópico "{topic}": {e}')
+                return 1
+        
+        return 0
     except Exception as e:
         print(f'Erro ao deletar o tópico: {e}')
         return 1
@@ -28,9 +45,8 @@ def main():
         parser.error('O parâmetro "topic_name" é obrigatório.')
     
     # Chama a função para deletar o tópico
-    delete_topic(admin_client, topic_name)
-    # exit_code = delete_topic(admin_client, topic_name)
-    # sys.exit(exit_code)
+    exit_code = delete_topic(admin_client, topic_name)
+    sys.exit(exit_code)
 
 if __name__ == '__main__':
     main()
