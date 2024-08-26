531830804d35ec6e76763e76a18b2b2a94e152e5
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-25 23:27:57 -0300

    Fix

--- kafka-confluent-delete.py
+++ kafka-confluent-delete.py
@@ -1,20 +1,32 @@
 import sys
 import argparse
 from confluent_kafka.admin import AdminClient
+from confluent_kafka.error import KafkaException
 
 def delete_topic(admin_client, topic_name):
-    # Deletar o tópico
     try:
         # Verifica se o tópico já existe
         metadata = admin_client.list_topics(timeout=10)
         if topic_name not in metadata.topics:
-            print(f"O tópico '{topic_name}' não existe.")
+            print(f'O tópico "{topic_name}" não existe.')
             return 1
         
-        admin_client.delete_topics([topic_name])
-        print(f'Tópico "{topic_name}" deletado com sucesso.')
+        # Deletar o tópico
+        futures = admin_client.delete_topics([topic_name])
+        
+        # Aguarde a conclusão da deleção
+        for topic, future in futures.items():
+            try:
+                future.result()  # Bloqueia até a deleção ser concluída
+                print(f'Tópico "{topic}" deletado com sucesso.')
+            except KafkaException as e:
+                print(f'Erro ao deletar o tópico "{topic}": {e}')
+                return 1
+        
+        return 0
     except Exception as e:
         print(f'Erro ao deletar o tópico: {e}')
+        return 1
 
 def main():
     # Configuração do cliente AdminClient
@@ -35,3 +47,6 @@ def main():
     # Chama a função para deletar o tópico
     exit_code = delete_topic(admin_client, topic_name)
     sys.exit(exit_code)
+
+if __name__ == '__main__':
+    main()
