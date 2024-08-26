7ed2db706516f0bc5f1c8604ed85c01028864829
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-25 22:07:19 -0300

    Fix

--- kafka-confluet-update.py
+++ kafka-confluet-update.py
@@ -1,8 +1,7 @@
 import argparse
 from confluent_kafka.admin import AdminClient, ConfigResource, ConfigEntry, AlterConfigOpType
 
-def get_config(topic_name, config_name):
-    admin_client = AdminClient({'bootstrap.servers': '13.92.98.80:9092'})
+def get_config(admin_client, topic_name, config_name):
     resource = ConfigResource('topic', topic_name)
     result_dict = admin_client.describe_configs([resource])
     config_entries = result_dict[resource].result()
@@ -12,9 +11,7 @@ def get_config(topic_name, config_name):
     else:
         raise ValueError(f"Configuration {config_name} not found for topic {topic_name}")
 
-def set_config(topic_name, config_dicts):
-    admin_client = AdminClient({'bootstrap.servers': '13.92.98.80:9092'})
-    
+def set_config(admin_client, topic_name, config_dicts):
     # Create ConfigEntry objects for each config to set
     config_entries = [ConfigEntry(name=name, value=str(value), incremental_operation=AlterConfigOpType['SET'])
                       for name, value in config_dicts.items()]
@@ -23,32 +20,32 @@ def set_config(topic_name, config_dicts):
     resource = ConfigResource('topic', topic_name, incremental_configs=config_entries)
     
     result_dict = admin_client.incremental_alter_configs([resource])
-    result_dict[resource].result()    
-  
-    # Set multiple configurations
-    set_config(topic_name, config_dicts) 
-    
+    result_dict[resource].result()  # Wait for the result to ensure the configuration is applied
+
 def main():
     # Configura o parser de argumentos
     parser = argparse.ArgumentParser(description='Atualiza a configuração de um tópico do Kafka.')
     parser.add_argument('topic_name', type=str, help='Nome do tópico')
-
-    args = parser.parse_args()
-
+    
     # Recebe os parâmetros da linha de comando
+    args = parser.parse_args()
     topic_name = args.topic_name
-
-    # Define configurations to be set
+    
+    # Configuração do cliente Kafka
+    admin_client = AdminClient({'bootstrap.servers': '13.92.98.80:9092'})
+    
+    # Define configurações para serem aplicadas
     config_dicts = {
         'retention.ms': 10,
-        'cleanup.policy': 'compact'    }
+        'cleanup.policy': 'compact'
+    }
     
-    # Set multiple configurations
-    set_config(topic_name, config_dicts)
-
-    # Check if the config property has been updated
+    # Define múltiplas configurações
+    set_config(admin_client, topic_name, config_dicts)
+    
+    # Verifica se as propriedades de configuração foram atualizadas
     for config_name in config_dicts.keys():
-        new_value = get_config(topic_name, config_name)
+        new_value = get_config(admin_client, topic_name, config_name)
         print(f'Now {config_name} for topic {topic_name} is {new_value}')
 
 if __name__ == '__main__':
