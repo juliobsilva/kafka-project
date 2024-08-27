5f672b0dd297f5620b7945701b8c41d6b37559f4
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-27 17:52:59 -0300

    Fix

--- kafka-permission-topic.py
+++ kafka-permission-topic.py
@@ -1,32 +1,29 @@
 import argparse
-from confluent_kafka.admin import AdminClient, ConfigResource, ConfigEntry, AlterConfigOpType, NewTopic, AclBinding,AclBinding, AclOperation, AclPermissionType, ResourceType
+from confluent_kafka.admin import AdminClient, AclBinding, AclOperation, AclPermissionType, ResourceType, ResourcePatternType
 
 def set_permission_topic(admin_client, topic_name, user_name):
-    
-    topic_name = topic_name
-    user_name = user_name
-
     # Definindo ACL para um tópico específico
-    acl_read = AclBinding (
+    acl_read = AclBinding(
         restype=ResourceType.TOPIC,
-        name='{topic_name}',
-        resource_pattern_type=None,  # Pode deixar como None ou configurar conforme necessário
-        principal="{user_name}",
+        name=topic_name,
+        resource_pattern_type=ResourcePatternType.LITERAL,  # Pode configurar conforme necessário
+        principal=f"User:{user_name}",
         host="*",
         operation=AclOperation.READ,  # Ou WRITE, ALL, etc.
         permission_type=AclPermissionType.ALLOW
     )
 
     # Cria as ACLs no Kafka
-    futures = admin_client.create_acls([acl_read])
-
-    # Verifica se as ACLs foram criadas com sucesso
-    for acl, future in futures.items():
-        try:
-            future.result()
-            print(f"Permissão {acl.operation} concedida com sucesso para {acl.principal} no tópico {acl.resource.pattern}")
-        except Exception as e:
-            print(f"Falha ao conceder permissão {acl.operation}: {e}")
+    try:
+        futures = admin_client.create_acls([acl_read])
+        for acl, future in futures.items():
+            try:
+                future.result()
+                print(f"Permissão {acl.operation} concedida com sucesso para {acl.principal} no tópico {acl.name}")
+            except Exception as e:
+                print(f"Falha ao conceder permissão {acl.operation}: {e}")
+    except Exception as e:
+        print(f"Erro ao criar ACLs: {e}")
 
 def main():
     # Configura o parser de argumentos
@@ -37,7 +34,6 @@ def main():
     args = parser.parse_args()
 
     # Recebe os parâmetros da linha de comando
-
     topic_name = args.topic_name
     user_name = args.user_name
 
@@ -46,3 +42,6 @@ def main():
 
     # Chama a função para conceder permissões
     set_permission_topic(admin_client, topic_name, user_name)
+
+if __name__ == "__main__":
+    main()
