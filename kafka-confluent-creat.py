d77545a78e3ebe5b6811407f4ac1d0b3ceae5a06
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-29 20:52:31 -0300

    Fix

--- kafka-confluent-creat.py
+++ kafka-confluent-creat.py
@@ -73,14 +73,12 @@ def main():
     parser = argparse.ArgumentParser(description='Cria e normaliza um nome de tópico do Kafka.')
     parser.add_argument('domain', type=str, help='Dominio')
     parser.add_argument('environment', type=str, help='Ambiente')
-    parser.add_argument('--date_type', type=str, help='Tipo do dado')
-    parser.add_argument('--date_name', type=str, help='Nome do dado')
-
-    # Parâmetros adicionais
-    parser.add_argument('--retention_ms', type=int, help='Tempo de retenção')
-    parser.add_argument('--max_message_bytes', type=int, help='Política de limpeza')
-    parser.add_argument('--num_partitions', type=int, help='Número de partições')
-    parser.add_argument('--replication_factor', type=int, help='Fator de replicação')
+    parser.add_argument('date_type', type=str, help='Tipo do dado')
+    parser.add_argument('date_name', type=str, help='Nome do dado')
+    parser.add_argument('retention_ms', type=int, help='Tempo de retenção')
+    parser.add_argument('max_message_bytes', type=int, help='Política de limpeza')
+    parser.add_argument('num_partitions', type=int, help='Número de partições')
+    parser.add_argument('replication_factor', type=int, help='Fator de replicação')
 
     args = parser.parse_args()    
 
