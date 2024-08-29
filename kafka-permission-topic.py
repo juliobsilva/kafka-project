7b69dffa794f55afabd4fa754575722c324386ce
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-29 10:43:49 -0300

    Fix

--- kafka-permission-topic.py
+++ kafka-permission-topic.py
@@ -25,6 +25,13 @@ def set_permission_topic(admin_client, topic_name, user_name):
     except Exception as e:
         print(f"Erro ao criar ACLs: {e}")
 
+    try:
+        acls = admin_client.describe_acls([acl_read])
+        for acl in acls:
+            print(f"ACL encontrada: {acl}")
+    except Exception as e:
+        print(f"Erro ao descrever ACLs: {e}")
+
 def main():
     # Configura o parser de argumentos
     parser = argparse.ArgumentParser(description='Concede permissões de leitura e escrita em um tópico do Kafka.')
