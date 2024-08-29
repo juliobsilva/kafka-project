fc44318378d83ddd84654678258ee57b233ae67a
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-29 11:32:10 -0300

    Fix

--- kafka-permission-topic.py
+++ kafka-permission-topic.py
@@ -42,7 +42,7 @@ def describe_acls(admin_client, topic_name, user_name):
         acl_filters = [acl_filter]  # Criar uma lista contendo o filtro
         acls = admin_client.describe_acls(acl_filters)
         for acl in acls:
-            print(f"ACL encontrada: {acl}")
+            print(f"ACL encontrada: {acl.principal} tem permissão de {acl.operation} no tópico {acl.name}")
     except Exception as e:
         print(f"Erro ao descrever ACLs: {e}")
 
