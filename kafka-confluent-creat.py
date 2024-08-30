c70b141f3b9520ac9c7c8759cd9b5cbabdc3072e
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-29 21:42:17 -0300

    Fix

--- kafka-confluent-creat.py
+++ kafka-confluent-creat.py
@@ -75,10 +75,10 @@ def main():
     parser.add_argument('environment', type=str, help='Ambiente')
     parser.add_argument('date_type', type=str, help='Tipo do dado')
     parser.add_argument('date_name', type=str, help='Nome do dado')
-    parser.add_argument('--retention_ms', type=int, help='Tempo de retenção', default=None)
-    parser.add_argument('--max_message_bytes', type=int, help='Política de limpeza', default=None)
-    parser.add_argument('--num_partitions', type=int, help='Número de partições', default=None)
-    parser.add_argument('--replication_factor', type=int, help='Fator de replicação', default=None)
+    parser.add_argument('retention_ms', type=int, help='Tempo de retenção')
+    parser.add_argument('max_message_bytes', type=int, help='Política de limpeza')
+    parser.add_argument('num_partitions', type=int, help='Número de partições')
+    parser.add_argument('replication_factor', type=int, help='Fator de replicação')
 
     args = parser.parse_args()    
 
