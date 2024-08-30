df8d69a77415efc3a86f7f21aa0f914d15e1c212
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-30 15:48:01 -0300

    add creatv2

--- kafka-confluent-creatv2.py
+++ kafka-confluent-creatv2.py
@@ -1,3 +1,4 @@
+import logging
 import json
 import os
 import sys
@@ -116,13 +117,10 @@ def main():
             set_default_config(admin_client, normalized_kafka_topic_name, config_dicts)
             sys.exit(create_result)
     else:
-        if any(param in (None, '', ' ') for param in [domain, environment, data_type, data_name]):
-            # Identifica qual parâmetro está faltando
-            for param, name in zip([domain, environment, data_type, data_name], ['domain', 'environment', 'date_type', 'date_name']):
-                if param in (None, '', ' '):
-                    print(f"O parâmetro {name} não foi informado")
-        else:
-            print("Não há tópico a ser criado")  
+         missing_params = [name for param, name in zip([domain, environment, data_type, data_name],['domain', 'environment', 'data_type', 'data_name']) if param in (None, '', ' ')]
+         if missing_params:
+             logging.error(f"Os seguintes parâmetros não foram informados: {', '.join(missing_params)}")
+             sys.exit(1)
 
 if __name__ == "__main__":
     main()
