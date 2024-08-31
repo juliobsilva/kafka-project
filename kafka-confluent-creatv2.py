fc588025bfa7ffd81eff80fc01474c0b1fc6cd48
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-08-31 17:47:26 -0300

    add creatv2

--- kafka-confluent-creatv2.py
+++ kafka-confluent-creatv2.py
@@ -75,15 +75,10 @@ def main():
     environment = os.getenv('ENVIRONMENT', '').strip()
     data_type = os.getenv('DATA_TYPE', '').strip()
     data_name = os.getenv('DATA_NAME', '').strip()
-    retention_ms = os.getenv('RETENTION_MS', 7200000)
-    max_message_bytes = os.getenv('MAX_MESSAGE_BYTES', 1048576)
-    num_partitions = os.getenv('NUM_PARTITIONS', 1)
-    replication_factor = os.getenv('REPLICATION_FACTOR', 1)
-
-    num_partitions = int(num_partitions)
-    replication_factor = int(replication_factor)
-    print(type(num_partitions))
-    print(type(replication_factor))
+    retention_ms = os.getenv('RETENTION_MS')
+    max_message_bytes = os.getenv('MAX_MESSAGE_BYTES')
+    num_partitions = os.getenv('NUM_PARTITIONS')
+    replication_factor = os.getenv('REPLICATION_FACTOR')
 
     config_dicts = {
         "retention.ms": "7200000",  
