080c176b731b9edfb6ecf5f14bedddec1a0bb2f9
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-09-03 09:03:46 -0300

    Fix

--- kafka-confluent-creatv2.py
+++ kafka-confluent-creatv2.py
@@ -145,7 +145,7 @@ def main():
                 logging.error(f"Erro ao criar o tópico Kafka: {e}")
                 sys.exit(1)
     
-    except (Exception, KafkaError) as e:
+    except Exception as e:
         logging.error(f"Erro inesperado no main: {e}")
         sys.exit(1)
 
